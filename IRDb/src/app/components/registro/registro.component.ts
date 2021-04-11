e2271fcc7cf807c554cfb93c0390a29a692d658a
mariana-p25 <mariana.pardo.a@gmail.com>
2021-04-10 22:50:10 -0500

    registro html y css

--- IRDb/src/app/components/registro/registro.component.ts
+++ IRDb/src/app/components/registro/registro.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registro',
@@ -7,9 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      nombre: ['', Validators.required],
+      correo: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmar: ['', [Validators.required, Validators.minLength(6)]]
+    }, {
+      validators: () => {
+        if (!this.form) return;
+        if (this.form.controls.password.value == this.form.controls.confirmar.value) {
+          return null;
+        } else {
+          return {
+            confirmPass: true
+          }
+        }
+      }
+    });
+  }
+
+  registrar() {
+    //TODO
   }
 
 }
