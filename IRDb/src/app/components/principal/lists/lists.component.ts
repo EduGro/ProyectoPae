c40bbece1e4e00d0b8fb5c6e0429eb0f4a21b5f8
EduGro <73360551+EduGro@users.noreply.github.com>
2021-04-11 23:34:26 -0500

    Revert "Revert "Added style to recipes, users and lists""
    This reverts commit b9c59d1873b74209c8c93e6819e272e741b77526.

--- IRDb/src/app/components/principal/lists/lists.component.ts
+++ IRDb/src/app/components/principal/lists/lists.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import { Router } from '@angular/router';
+
+interface List{
+  name:string;
+  id:number;
+}
 
 @Component({
   selector: 'app-lists',
   templateUrl: './lists.component.html',
   styleUrls: ['./lists.component.scss']
 })
+
 export class ListsComponent implements OnInit {
 
-  constructor() { }
+  @Input() lists:List[] = [{name:"List1",
+    id:1}, {name:"List2",
+    id:2}, {name:"List3",
+    id:3}
+  ];
+
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  selectList(list:List){
+    this.router.navigate(['./lists/:'+list.id]);
+  }
 }
--- IRDb/src/app/components/principal/recipes/recipes.component.ts
+++ IRDb/src/app/components/principal/recipes/recipes.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-recipes',
@@ -6,6 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes.component.scss']
 })
 export class RecipesComponent implements OnInit {
+
+  @Input() recipe = {
+    name: 'Flan',
+    prep_time: '20 min',
+    cook_time: '60 min',
+    servings: '8',
+    cuicine: 'italiana',
+    ingredients: [
+      "3/4 cup sugar",
+      "1/4 cup water",
+      "1 package (8 ounces) cream cheese, softened",
+      "5 large eggs",
+      "1 can (14 ounces) sweetened condensed milk",
+      "1 can (12 ounces) evaporated milk",
+      "1 teaspoon vanilla extract"
+    ],
+    steps: [
+      "In a heavy saucepan, cook sugar and water over medium-low heat until melted and golden, about 15 minutes. Brush down crystals on the side of the pan with additional water as necessary. Quickly pour into an ungreased 2-qt. round baking or souffle dish, tilting to coat the bottom; let stand for 10 minutes.",
+      "Preheat oven to 350°. In a bowl, beat the cream cheese until smooth. Beat in eggs, 1 at a time, until thoroughly combined. Add remaining ingredients; mix well. Pour over caramelized sugar.",
+      "Place the dish in a larger baking pan. Pour boiling water into larger pan to a depth of 1 in. Bake until the center is just set (mixture will jiggle), 50-60 minutes.",
+      "Remove dish from a larger pan to a wire rack; cool for 1 hour. Refrigerate overnight.",
+      "To unmold, run a knife around edges and invert onto a large rimmed serving platter. Cut into wedges or spoon onto dessert plates; spoon sauce over each serving."
+    ]
+  }
 
   constructor() { }
 
