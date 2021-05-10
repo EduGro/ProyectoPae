919f521850e595a54b4134b5691933dd08218f38
EduGro <73360551+EduGro@users.noreply.github.com>
2021-05-10 02:22:46 -0500

    make it bette22

--- IRDb/server.js
+++ IRDb/server.js
@@ -3,22 +3,22 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist/IRDb/index.html'));
+app.use(express.static(__dirname + '/dist/IRDb/'));
 
 app.get('*', (req,res,next) => {
-    res.sendFile(path.join(__dirname + '/dist/IRDb/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/IRDb/'));
 });
 
 app.get('/', (req,res,next) => {
-    res.sendFile(path.join(__dirname + '/dist/IRDb/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/IRDb/'));
 });
 
 app.get('', (req,res,next) => {
-    res.sendFile(path.join(__dirname + '/dist/IRDb/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/IRDb/'));
 });
 
 app.get('/*', (req,res,next) => {
-    res.sendFile(path.join(__dirname + '/dist/IRDb/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/IRDb/'));
 });
 
 
