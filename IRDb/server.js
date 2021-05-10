91e2d2402b2c840258e1a8e4702c5d5369bb7aa4
EduGro <73360551+EduGro@users.noreply.github.com>
2021-05-09 23:13:45 -0500

    make it better6

--- IRDb/server.js
+++ IRDb/server.js
@@ -5,8 +5,8 @@ const app = express();
 
 app.use(express.static(__dirname + '/dist/index.html'));
 
-app.get('/*', (req,res,next) => {
-    res.sendFile(path.join(__dirname + '/dist/dictionary/index.html'));
+app.get('*', (req,res,next) => {
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
 
